0b23bc283d6f337753890fa80aab2c9abe1b0c8e
alexkors <korsun55555@mail.ru>
2022-07-01 22:39:54 +0700

    старт работы с кодом

--- introduction_course_task.cs
+++ introduction_course_task.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Контрольное задание.
+// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3.
+
+Console.WriteLine("Пожалуйста, введите элементы исходного массива через пробел.");
+string[] generalArray = Console.ReadLine().Split().ToArray();
+
+Console.WriteLine();
+Console.Write("Искомый массив: ");
+ArrayOutput(GetShortElementArray(generalArray));
